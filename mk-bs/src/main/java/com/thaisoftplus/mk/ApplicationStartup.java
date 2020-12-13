bb443ca714421221713d69d82ec06ea82cfc9fc9
wittakarn <wittakarn.expert@gmail.com>
2020-12-13 15:57:31 +0700

    Initial mk project

---
+++ mk-bs/src/main/java/com/thaisoftplus/mk/ApplicationStartup.java
@@ -0,0 +1,15 @@
+package com.thaisoftplus.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStartup {
+    /**
+     * The main method.
+     * @param args the arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStartup.class, args);
+    }
+}
